4e3ab3e96f78c73167bac61dd5a1662b0b068472
vivek sharma <vivekslim@gmail.com>
2021-07-20 13:14:56 +0530

    CRUD

--- block-BNaacp/sample/app.js
+++ block-BNaacp/sample/app.js
@@ -24,19 +24,84 @@ app.get("/" , (req,res)=> {
     res.send("Welcome")
 })
 
-app.post("/users" , (req,res)=>{
+app.post("/users" , (req,res,next)=>{
     User.create(req.body , (err,user)=> {
+       // console.log(err,user)
+       next(err)
+       res.json(user)
+    })
+})   //create file
+
+
+app.get("/users" , (req,res,next)=> {
+    User.find({}, (err,users)=> {
+        next(err)
+        res.json({users:users})  // User.find give array , therefore, we put object symbol inside res.json
+    })
+}) // Read or find file
+
+/*Q. query a single document(user) using mongoose
+
+on GET request on '/users/:id' route, fetch a user
+use Model.find({_id: 'some-id'}) // use _id of inserted document in database
+use Model.findOne({_id: 'some-id'})
+use Model.findById(id) */
+
+app.get("/users/:id" , (req,res,next)=> {
+    var id = req.params.id
+    /*User.findById(id , (err,user)=> {
+        next(err)
+        res.json(user)
+    })*/
+
+    /*User.find({_id:"60f6775ac07f980e086e7b00"},(err,user)=> {
+        next(err)
+        res.json(user)
+    })*/
+
+    User.findOne({_id:"60f6775ac07f980e086e7b00"},(err,user)=> {
         next(err)
         res.json(user)
     })
 })
 
 
-// error handle middleware
+/*Update a user
 
-app.use((err,req,res,next)=> {
-    res.send(err)
+on PUT request on '/users/:id', update a user
+use Model.update
+use Model.updateOne
+use Model.findByIdAndUpdate(id) */
+
+
+app.put("/users/:id" , (req,res,next)=> {
+    var id = req.params.id
+    User.findByIdAndUpdate(id,req.body, {new:true} , (err, updateduser)=> {
+        next(err)
+        res.json(updateduser)
+    })
 })
+
+
+/*Q. delete a user using Model.findByIdAndDelete()
+
+on DELETE request on '/users/:id' */
+
+
+app.delete("/users/:id" , (req,res)=> {
+    Product.findByIdAndDelete(req.params.id , (err , deleteduser)=> {
+        res.send(`${deleteduser.name} was deleted`)
+    })
+})
+
+
+
+// error handle middleware
+app.use((err,req,res,next)=> {
+    
+    res.status(500).send(err)
+ })
+
 
 //listener
 
